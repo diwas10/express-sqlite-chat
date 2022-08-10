f00610230d952483c2f13724f662f161158514c7
Diwash Baral <diwasbaral60@gmail.com>
2022-08-10 07:34:50 +0545

    add: socket connection event

--- src/core/Socket/socket.controller.ts
+++ src/core/Socket/socket.controller.ts
@@ -1,28 +1,37 @@
 import SocketIo from 'socket.io';
 import { Server } from 'http';
 
-class SocketController {
+class Socket {
 	public static io: SocketIo.Server;
-	private readonly instance: SocketController;
+	private readonly instance: Socket;
 
 	constructor(server: Server) {
-		this.initSocketMiddleware();
-		SocketController.io = new SocketIo.Server(server, {
+		if (this.instance) return this.instance;
+		this.init(server);
+		this.instance = this;
+	}
+
+	init(server: Server): void {
+		Socket.io = new SocketIo.Server(server, {
 			cors: {
 				origin: '*',
 				methods: ['GET', 'POST'],
 			},
 		});
 
-		if (this.instance) return this.instance;
-		this.instance = this;
+		this.initSocketMiddleware();
+
+		Socket.io.on('connection', (socket) => {
+			console.log('************** User Connected **************');
+		});
 	}
 
+
 	initSocketMiddleware(): void {
-		SocketController.io.use((socket, next) => {
-			console.log(socket.handshake.headers);
+		Socket.io.use((socket, next) => {
+			console.log(socket.handshake.headers, 'dadasdasdasdasdasdasdas');
 		});
 	}
 }
 
-export default SocketController;
+export default Socket;
